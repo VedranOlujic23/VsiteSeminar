1cd2b8221aa5fcce9846687288f29695b63ebb53
Vedran Olujic <vedranolujic23@gmail.com>
2022-02-07 22:54:12 +0100

    Implemented main form and ping response

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Text;
+
+
+
+namespace VsiteSeminar
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// Ping
+        ///Napraviti GUI program koji će u zadanim vremenskim intervalima
+        ///provjeravati dostupnost neke IP adrese(slanjem PING upita) te
+        ///prikazivati vrijeme odziva za svaki upit.Program treba omogućiti
+        ///zadavanje IP adrese i duljine intervala između pojedinih upita u
+        ///sekundama.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.Run(new Form1());
+
+
+
+        }
+    }
+
+
+
+}
