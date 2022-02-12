7507f2a9650e418061cbe3ff6e6c36f5c0b2d909
Vedran Olujic <vedranolujic23@gmail.com>
2022-02-12 17:34:50 +0100

    Implemented timer tick event for periodic ping calls, needs fix for changing ping and IP address after timer started

--- Form1.cs
+++ Form1.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Net.NetworkInformation;
+using System.Windows.Forms;
 
 namespace VsiteSeminar
 {
 
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
@@ -21,18 +15,13 @@ namespace VsiteSeminar
 
         private void StartPingMeasurement_Click(object sender, EventArgs e)
         {
-            Ping p = new Ping();
-            PingReply r;
-            string s;
+            string IPAddress;
+            IPAddress = textBoxIPAdress.Text;
+            string PingPeriod;
+            
+            timer1.Enabled = true;
 
-            s = textBoxIPAdress.Text;
-            r = p.Send(s);
 
-            if (r.Status == IPStatus.Success)
-            {
-                richTextBox1.Text = "Ping to " + s.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
-                   + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n";
-            }
         }
 
         private void textBox1_Validated(object sender, EventArgs e)
@@ -42,7 +31,7 @@ namespace VsiteSeminar
                 MessageBox.Show("Please use valid IP or web address!!");
             }
         }
-    
+
 
 
         private void Form1_Load(object sender, EventArgs e)
@@ -65,7 +54,45 @@ namespace VsiteSeminar
 
         }
 
+        private void MeasurePing(object sender, EventArgs e)
+        {
 
+            Ping p = new Ping();
+            PingReply r;
+            string IPAddress;
+            IPAddress = textBoxIPAdress.Text;
+            string PingPeriod;
+            PingPeriod = textBoxPingPeriod.Text;
+
+            r = p.Send(IPAddress);
+
+            if (r.Status == IPStatus.Success)
+            {
+                richTextBox1.Text = "Ping to " + IPAddress.ToString() + "[" + r.Address.ToString() + "]" + " Successful"
+                   + " Response delay = " + r.RoundtripTime.ToString() + " ms" + "\n" + "Ping period (s) = " + (timer1.Interval/1000).ToString() ;
+            }
+
+
+        }
+
+        private void textBoxPingPeriod_TextChanged(object sender, EventArgs e)
+        {
+           // if (textBoxPingPeriod.Text == "")
+                
+           //     timer1.Interval = int.Parse(textBoxPingPeriod.Text) * 1000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var send = sender;
+            var eventArgument = e;
+            MeasurePing(send, eventArgument);
+        }
+
+        private void StopPingMeasurement_click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
     }
 }
 
